fa68b2d8997bd6eb279ca47279b8bff1e03f985d
Polonkai Pál <polonkaipal5@gmail.com>
2019-10-30 11:32:55 +0100

    Két szám összege

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello Pali!\n");
+    // Két szám összeadása
+    printf("Add meg az első számot:\n");
+    int x;
+    scanf("%d", &x);
+    printf("Add meg a második számot:\n");
+    int y;
+    scanf("%d", &y);
+    int s=x;
+    while(y>0){
+        s++;
+        y--;
+    }
+    printf("A két szám összege: %d", s);
     return 0;
 }
