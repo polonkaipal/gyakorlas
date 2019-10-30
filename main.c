24cb451e455153d211e2f01a480a15556620dc27
Polonkai Pál <polonkaipal5@gmail.com>
2019-10-30 12:24:42 +0100

    Szökőév meghatározása

--- main.c
+++ main.c
@@ -3,6 +3,25 @@
 
 int main()
 {
-    printf("Hello Pali!\n");
+    // Szökőév meghatározása
+    printf("Add meg az évszámot:\n");
+    int ev;
+    scanf("%d", &ev);
+    if(ev%4==0){
+        if(ev%100==0){
+            if(ev%400==0){
+                printf("Szökőév");
+            }
+            else{
+                printf("Nem szökőév");
+            }
+        }
+        else{
+            printf("Szökőév");
+        }
+    }
+    else{
+        printf("Nem szökőév");
+    }
     return 0;
 }
