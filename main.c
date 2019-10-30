edaa22ea1f2dd20ab83546cba7ce5bd84ad66198
Polonkai Pál <polonkaipal5@gmail.com>
2019-10-30 12:43:47 +0100

    Faktoriális meghatározása

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello Pali!\n");
+    // Faktoriális
+    printf("Adj meg egy pozitív egész számot:\n");
+    int szam;
+    scanf("%d", &szam);
+    int seged=1;
+    while(szam>1){
+        seged*=szam--;
+    }
+    printf("A szám faktoriálisa: %d", seged);
     return 0;
 }
