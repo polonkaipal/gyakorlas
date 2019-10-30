9a666a23429b8cd4d8dd5a2042856cf5a4e48e90
Polonkai Pál <polonkaipal5@gmail.com>
2019-10-30 12:35:22 +0100

    Hatványozás (alap és a kitevő pozitív egész szám)

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello Pali!\n");
+    // Hatványozás (alap és a kitevő pozitív egész szám)
+    printf("Add meg a hatvány alapot (pozitív egész szám):\n");
+    int alap;
+    scanf("%d", &alap);
+    printf("Add meg a hatvány kitevőt (pozitív egész szám):\n");
+    int kitevo;
+    scanf("%d", &kitevo);
+    int szorzat=alap;
+    while(kitevo>1){
+        szorzat*=alap;
+        kitevo--;
+    }
+    printf("A keresett hatvány: %d", szorzat);
     return 0;
 }
