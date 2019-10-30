e9b18acae99bb9c988da699a34bcbc2874b9ce27
Polonkai Pál <polonkaipal5@gmail.com>
2019-10-30 11:27:36 +0100

    Előjel függvény

--- main.c
+++ main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello Pali!\n");
+    // Előjel függvény
+    printf("Adj meg egy számot\n");
+    int szam;
+    scanf("%d", &szam);
+    if(szam<0){
+        printf("-1");
+    }
+    else if(szam==0){
+        printf("0");
+    }
+    else{
+        printf("1");
+    }
     return 0;
 }
