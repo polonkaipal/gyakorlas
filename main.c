081ac2c4ca19edaa707c7d690aa838c658261e4c
Polonkai Pál <polonkaipal5@gmail.com>
2019-10-30 12:50:27 +0100

    Elsőfokú egyenlet megoldása

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello Pali!\n");
+    // Elsőfokú egyenlet megoldása
+    printf("Add meg az 'a' értékét:\n");
+    int a;
+    scanf("%d", &a);
+    printf("Add meg a 'b' értékét:\n");
+    int b;
+    scanf("%d", &b);
+    printf("Az elsőfokú egyenlet megoldása: %f", (float)-b/a);
     return 0;
 }
