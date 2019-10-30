73509d810a0aedea4d9f49b5fa421abcc4f43b8c
Polonkai Pál <polonkaipal5@gmail.com>
2019-10-30 12:03:09 +0100

    Három szám maximuma

--- main.c
+++ main.c
@@ -3,6 +3,33 @@
 
 int main()
 {
-    printf("Hello Pali!\n");
+    // Három szám maximuma
+    printf("Add meg az első számot:\n");
+    int a;
+    scanf("%d", &a);
+    printf("Add meg a második számot:\n");
+    int b;
+    scanf("%d", &b);
+    printf("Add meg a harmadik számot:\n");
+    int c;
+    scanf("%d", &c);
+    int R;
+    if(a>c){
+        if(a>b){
+            R=a;
+        }
+        else{
+            R=b;
+        }
+    }
+    else{
+        if(b>c){
+            R=b;
+        }
+        else{
+            R=c;
+        }
+    }
+    printf("A legnagyobb szám: %d", R);
     return 0;
 }
